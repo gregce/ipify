2f80f2ab7648c72f512dad17a0c8b42847f600ff
gregce <gregce@dropbox.com>
2017-05-25 15:35:58 -0700

    0.1.0 initial commit of ipify for R

---
+++ R/ipify.R
@@ -0,0 +1,42 @@
+#' Get a response object from ipify
+#'
+#' This is a private, non-exported function
+#'
+#' @name get_ip_response
+#' @importFrom methods hasArg
+
+
+get_ip_response <- function(url = ipify.env$API_URL, ...) {
+  args <- list(...)
+  if (hasArg(format)) {
+    if (args$format %in% c("text","json","jsonp")) {
+    url <- paste0(url,"?format=",args$format, sep="")
+    } else
+      message(paste0(args$format, " is not a recognized return format, defaulting to text"))
+  }
+  return(httr::GET(url = url, httr::add_headers("user-agent" = ipify.env$USER_AGENT)))
+}
+
+#' Get IP
+#'
+#' Query the ipify service (https://www.ipify.org) to retrieve this machine's
+#' public IP address.
+#'
+#' @name get_ip
+#' @param max_tries The number of times to retry the ipify service on failure. Defaults to 3.
+#' @param format Whether you want the service to return. Valid options = c("text", "json", "jsonp")
+#' @export
+#' @return The public IP address of this machine as a plain text string,
+
+get_ip <- function(max_tries = ipify.env$MAX_TRIES, ...) {
+  for(i in 1:max_tries){
+    tryCatch({
+      resp <- get_ip_response(...)
+      break
+    },  error=function(cond) message(paste0("Error accessing the ipify service", cond))
+    )
+  }
+  if (resp$status_code!=200) stop(paste0("Received an invalid status code from ipify: ", resp$status_code))
+  return(httr::content(resp))
+}
+
---
+++ R/settings.R
@@ -0,0 +1,36 @@
+#' Function to get the users OS for setting the user agent
+#'
+#' This is a private, non-exported function
+#'
+#' @name get_os
+#'
+get_os <- function(){
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)){
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "osx"
+  } else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "OS_X"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+    ## add check for Windows
+  }
+  tolower(os)
+}
+
+
+#' Set up the environment used to store setting information
+ipify.env <-new.env(parent = emptyenv())
+
+ipify.env$API_URL <- "https://api.ipify.org"
+
+ipify.env$MAX_TRIES <- 3
+
+ipify.env$USER_AGENT <- sprintf("r-ipify/%s rlang/%s %s", version$nickname, version$minor, get_os())
+
+
+
+get_os()
